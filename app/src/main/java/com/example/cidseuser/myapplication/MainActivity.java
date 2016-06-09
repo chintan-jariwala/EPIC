b767e6e475e8e44bc0bd95a8added51348466ea1
coolArchu <chaudhari.carlhayden@gmail.com>
2016-06-09 12:02:13 -0700

    complete code for user info for profile setup

--- app/src/main/java/com/example/cidseuser/myapplication/MainActivity.java
+++ app/src/main/java/com/example/cidseuser/myapplication/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.cidseuser.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
-//import android.widget.TextView;
+import android.widget.TextView;
 //import android.app.*;
 //import android.widget.TextView;
 import org.json.JSONArray;
@@ -20,10 +22,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity
 {
+    SharedPreferences sharedPreferences;
+    public   String myPreference = "myPref";
+    public   String name = "nameKey";
+    public   String phone = "phoneKey";
+    public  String address ="addressKey";
+    public   String city ="cityKey";
+    public   String familyName ="familyNameKey";
+    public  String familyPhone ="familyPhoneKey";
+
     //private TextView;
     private EditText etName;
     private EditText etPhone;
@@ -48,12 +62,26 @@ public class MainActivity extends AppCompatActivity
         //TextView view;
         //view = setText("Your Profile Info: \n" + etName.getText().toString()+ "\n"+ etPhone.getText().toString());
 
-        etName.getText();
-        etPhone.getText();
-        etAddress.getText();
-        etCity.getText();
-        etFamilyName.getText();
-        etFamilyPhone.getText();
+        name = etName.getText().toString();
+        phone = etPhone.getText().toString();
+        address = etAddress.getText().toString();
+        city = etCity.getText().toString();
+        familyName = etFamilyName.getText().toString();
+        familyPhone = etFamilyPhone.getText().toString();
+
+        Toast.makeText(this,name,Toast.LENGTH_LONG).show();
+        sharedPreferences = getSharedPreferences(myPreference, 0);
+        Editor editor = sharedPreferences.edit();
+        editor.putString("etName", name);
+        editor.putString("etPhone", phone);
+        editor.putString("etAddress", address);
+        editor.putString("etCity", city);
+        editor.putString("etFamilyName", familyName);
+        editor.putString("etFamilyPhone", familyPhone);
+
+        editor.commit();
+
+
 
     }
 
@@ -73,26 +101,22 @@ public class MainActivity extends AppCompatActivity
 
         initializeVariables();
 
-
         btnProfileInfoSubmit.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
                 onSubmitProfileButton();
+                etName.setText(null);
 
             }
+
+
         });
-    }
-
-    /**
-     *
-     */
-    private void parseJsonFile()
-    {
-        String jsonString = readJsonFile();
 
     }
+
+
 
 
 
