37e095e25937a6ee0effcc08c7eec5016f82026d
coolArchu <chaudhari.carlhayden@gmail.com>
2016-06-14 09:49:32 -0700

    Fixed Merge issues

---
+++ app/src/main/java/com/example/cidseuser/myapplication/CameraForEpic.java
@@ -0,0 +1,62 @@
+package com.example.cidseuser.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+/**
+ * Created by cidseuser on 6/14/2016.
+ */
+public class CameraForEpic extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_for_epic);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    public void buttonOnClick (View v)
+    {
+        dispatchTakePictureIntent();
+    }
+
+
+}
--- app/src/main/java/com/example/cidseuser/myapplication/MainActivity.java
+++ app/src/main/java/com/example/cidseuser/myapplication/MainActivity.java
@@ -1,23 +1,12 @@
 package com.example.cidseuser.myapplication;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> remotes/origin/slidshow
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-<<<<<<< HEAD
+
 import android.os.Handler;
-import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
-import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity
 {
@@ -46,57 +35,5 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-=======
-import android.view.animation.Animation;
 
-public class MainActivity extends AppCompatActivity {
-
-
-
-    public void onCreateProfileSetUp()
-    {
-
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-
-
-    }
->>>>>>> remotes/origin/slidshow
 }
